63447e0bc638ad8e5b5b8f938e5017012b5a79b9
Thomas <youngkeu@buffalo.edu>
2016-10-31 18:33:48 -0400

    edited problems after git merge conflict

--- design.js
+++ design.js
@@ -11,20 +11,14 @@ function clock(){
     day = date.getDay();
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     h = date.getHours();
-<<<<<<< HEAD
-=======
     var amOrPm = "AM";
->>>>>>> 6d9d8eec9b95c58929290890fc7e2a7a9f836101
     if(h<10)
     {
         h = "0"+h;
     }
     else if(h>12){
     	h = h%12;
-<<<<<<< HEAD
-=======
         amOrPm = "PM";
->>>>>>> 6d9d8eec9b95c58929290890fc7e2a7a9f836101
     }
     m = date.getMinutes();
     if(m<10)
@@ -37,11 +31,8 @@ function clock(){
     //         s = "0"+s;
     // }
     dateToDisplay= months[month]+' '+ todaysDate+' '+days[day] +' '+year;
-<<<<<<< HEAD
     clockToDisplay = h+':'+m;
-=======
     clockToDisplay = h+':'+m+ ' ' + amOrPm;
->>>>>>> 6d9d8eec9b95c58929290890fc7e2a7a9f836101
     document.getElementById("date").innerHTML = dateToDisplay;
     document.getElementById("clock").innerHTML = clockToDisplay;
     setTimeout(clock,'1000');
@@ -52,10 +43,7 @@ function display(){
 	clock();
 }
 
-<<<<<<< HEAD
-=======
 function myBio(){
     document.getElementById("background").innerHTML = "I'm a Computer Science major.";
 }
->>>>>>> 6d9d8eec9b95c58929290890fc7e2a7a9f836101
 display();
