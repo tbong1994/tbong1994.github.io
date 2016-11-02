9fc15fbe08be57a00bba9e5e886597e4c683e23b
tbong1994 <youngkeu@buffalo.edu>
2016-11-02 09:51:38 -0400

    added pokemonhunt on projects tab

---
+++ projects.js
@@ -0,0 +1,12 @@
+
+
+function myProjects(){
+    document.getElementById("projects").innerHTML = "<br>"+"This game is a 2d game and written completely in Javascript and HTML."+
+    "<br>I mainly used Phaser.js library for the game physics and sprite animations."+
+    "<br>This project is still ongoing and I'm constantly developing this game to make it more fun to play"+
+    "<br>Pokemon Hunt is structured a little bit like Super Marios, but the user has to attack Pokemons and level up"+
+    "<br>otherwise, the user gets attacked by them and eventually die. There is an instruction on how to play in the game"+
+    "<br>You should try and play!";
+}
+
+myProjects();
