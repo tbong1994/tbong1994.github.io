b85fe310fffc5bb6b77a7d5f6f03b3254bad0d8c
Thomas <youngkeu@buffalo.edu>
2016-11-13 22:13:48 -0500

    working on design

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 
 
 function myBio(){
-    document.getElementById("background").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen."+
-    "<br>New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016."+
-    "<br>Apart from the academics, I enjoy playing sports. "+
-    "<br>I've met great people throughout my life and my family and friends are the most important people to me.";
+    document.getElementById("divBackground").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen."+
+    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016."+
+    "Apart from the academics, I enjoy playing sports. "+
+    "I've met great people throughout my life and my family and friends are the most important people to me.";
 }
 
 myBio();
--- interests.js
+++ interests.js
@@ -1,17 +1,17 @@
 
 
 function myInterests(){
-    document.getElementById("interests").innerHTML = "As a Computer Scientist, I am interested in Software Engineering/Developing."+
-    "<br>As you may see from the 'project' tab I have done and still am developing software, be it a game or web application."+
-    "<br>My favorite programming language is Java, one of the reasons is because it was my first programming language to learn so I became very attached to Java."+
-    "<br> Also, for me, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in."+
-    "<br>But I also like Javascript, Python and other programming languages."+"<br><br><br>"+
+    document.getElementById("divInterests").innerHTML = "As a Computer Scientist, I am interested in Software Engineering/Developing."+
+    "As you may see from the 'project' tab I have done and still am developing software, be it a game or web application."+
+    "My favorite programming language is Java, one of the reasons is because it was my first programming language to learn so I became very attached to Java."+
+    "Also, for me, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in."+
+    "But I also like Javascript, Python and other programming languages."+"<br><br><br>"+
     "Computer programming is one of the few things in life which allows you to start from scratch and come up with something very powerful."+
-    "<br>You not only have fun while programming to make something, but you also can accomplish a lot of things depending on what you want to achieve."+
-    "<br>but you also can accomplish a lot of things depending on what you want to achieve."+
-    "<br>My perspective of programming is that you can create software that everybody could benefit from."+
-    "<br>programming can not only make programmers great but the software they make can improve all of our lives."+
-    "<br>If what I create could help somebody else, I'd have accomplished all.";
+    "You not only have fun while programming to make something, but you also can accomplish a lot of things depending on what you want to achieve."+
+    "but you also can accomplish a lot of things depending on what you want to achieve."+
+    "My perspective of programming is that you can create software that everybody could benefit from."+
+    "programming can not only make programmers great but the software they make can improve all of our lives."+
+    "If what I create could help somebody else, I'd have accomplished all.";
 }
 
 myInterests();
