40b99d1f4d4143b324c8320af00c57fd06b84204
TB <youngkeu@buffalo.edu>
2016-11-30 14:35:10 -0500

    added programming language logos to my interests page.

--- interests.js
+++ interests.js
@@ -2,16 +2,21 @@
 
 function myInterests(){
     document.getElementById("divInterests").innerHTML = "As a Computer Scientist, I am interested in Software Engineering/Developing."+
-    "As you may see from the 'project' tab I have done and still am developing software, be it a game or web application."+
-    "My favorite programming language is Java, one of the reasons is because it was my first programming language to learn so I became very attached to Java."+
-    "Also, for me, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in."+
-    "But I also like Javascript, Python and other programming languages."+"<br><br><br>"+
-    "Computer programming is one of the few things in life which allows you to start from scratch and come up with something very powerful."+
-    "You not only have fun while programming to make something, but you also can accomplish a lot of things depending on what you want to achieve."+
-    "but you also can accomplish a lot of things depending on what you want to achieve."+
-    "My perspective of programming is that you can create software that everybody could benefit from."+
-    "programming can not only make programmers great but the software they make can improve all of our lives."+
+    "As you may see from the 'project' tab I have done and still am developing software, be it a game or web application. "+
+    "My favorite programming language is Java, one of the reasons is because it was my first programming language to learn so I became very attached to Java. "+
+    "Also, for me, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in. "+
+    "But I also like Javascript and Python. "+"<br><br><br>"+
+    "Computer programming is one of the few things in life which allow you to start from scratch and come up with something very powerful and useful. "+
+    "You not only enjoy while programming to make something, but you also can accomplish a lot of things depending on what you want to achieve. "+
+    "but you also can accomplish a lot of things depending on what you want to achieve. "+
+    "My perspective of programming is that you can create software that everybody could benefit from. "+
+    "programming can not only make programmers great but the software they make can improve all of our lives. "+
     "If what I create could help somebody else, I'd have accomplished all.";
 }
 
+function nonAcademicInterests(){
+    document.getElementById("divNonAcademicInterests").innerHTML="Apart from computers, I enjoy playing sports.";
+}
+
 myInterests();
+nonAcademicInterests();
--- projects.js
+++ projects.js
@@ -2,9 +2,9 @@
 function myProjects(){
     document.getElementById("divProjects").innerHTML = "This game is a 2d game and written completely in Javascript and HTML."+
     "I mainly used Phaser.js library for the game physics and sprite animations."+
-    "This project is still ongoing and I'm constantly developing this game to make it more fun to play"+
-    "Pokemon Hunt is structured a little bit like Super Marios, but the user has to attack Pokemons and level up"+
-    "otherwise, the user gets attacked by them and eventually die. There is an instruction on how to play in the game"+
+    "This project is still ongoing and I'm constantly developing this game to make it more fun to play! "+
+    "Pokemon Hunt is structured a little bit like Super Marios, but the user has to attack Pokemons and level up. "+
+    "Otherwise, the user gets attacked by them and eventually die. There is an instruction on how to play in the game. "+
     "You should try and play!";
 }
 
