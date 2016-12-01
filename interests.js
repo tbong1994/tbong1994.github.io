082c1ce80131ce168cbbcdb4ef3e7ef97f474522
TB <youngkeu@buffalo.edu>
2016-12-01 16:19:12 -0500

    edited ub logo and added more logos to interst page

--- interests.js
+++ interests.js
@@ -1,14 +1,15 @@
 
 
-function myInterests(){
-    document.getElementById("divInterests").innerHTML = "As a Computer Scientist, I am interested in Software Engineering/Developing. "+
+function paragraphs(){
+    document.getElementById("paragraphOne").innerHTML = "As a Computer Scientist, I am interested in Software Engineering/Developing. "+
     "As you may see from the 'project' tab I have done and still am developing software, be it a game or web application. "+
     "My favorite programming language is Java, one of the reasons is because it was my first programming language to learn so I became very attached to Java. "+
-    "Also, for me, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in. "+
+    "Also, Java was the most eligible, writable, but also powerful language for a beginner as myself to start programming in. "+
     "But I also like Javascript and Python. "+"I do have preferences but I do appreciate the differences among various programming languages. "+
     "The variety of the languages allows us to be educated which languages suit which situations. This is one of the reasons why programming languages could be efficient,useful and powerful. "+
-    "<br><br><br>"+
-    "Computer programming is one of the few things in life which allow you to start from scratch and come up with something very powerful and useful. "+
+    "<br><br>";
+
+    document.getElementById("paragraphThree").innerHTML = "Computer programming is one of the few things in life which allow you to start from scratch and come up with something very powerful and useful. "+
     "You not only enjoy while programming to make something, but you also could accomplish a lot of things depending on what you want to achieve. "+
     "My perspective of programming is that you should create software that everybody could benefit from. "+
     "Programming can not only make programmers great but the software we make could improve all of our lives. "+
@@ -19,5 +20,5 @@ function myInterests(){
 //     document.getElementById("divNonAcademicInterests").innerHTML="Apart from computers, I enjoy playing sports.";
 // }
 
-myInterests();
+paragraphs();
 // nonAcademicInterests();
--- projects.js
+++ projects.js
@@ -5,13 +5,14 @@ function myProjects(){
     "This project is still ongoing and I'm constantly developing this game to make it more fun to play! "+
     "Pokemon Hunt is structured a little bit like Super Marios, but the user has to attack Pokemons and level up. "+
     "Otherwise, the user gets attacked by them and eventually die. There is an instruction on how to play in the game. "+
-    "You should try!";
+    "You should try! "+
+    "You can download the source code from the link above.";
 }
 
 //SOURCE WEBSITES FOR THE PROJECT.
-function sources(){
-	document.getElementById("sources").innerHTML="<br>"+
-	""
-}
+// function description(){
+// 	document.getElementById("description").innerHTML="Pokemon Hunt gameplay";
+// }
 
 myProjects();
+// description();
