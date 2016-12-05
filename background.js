b01d9c0a8b0ae427940586b5480a68b59dd9ebc1
TB <youngkeu@buffalo.edu>
2016-12-04 22:16:17 -0500

    adding paragraphs for my background page

--- background.js
+++ background.js
@@ -8,11 +8,13 @@ function myBio(){
     "If I'm being very lazy, I'd watch movies all day. My favorite movie/book is Harry Potter. Watching Harry Potter movies makes me happy for some reason... "+
     "J.K Rowling is a genius. Who doesn't love Harry Potter, right?";
 
-    document.getElementById("what").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
+    document.getElementById("what").innerHTML = "There are lots of things that interest me. From the top of my head, the first on my list is "+
+    "movies/books. Without movies/books, we are nothing. There would not be any imagination or education. To be honest, I'm not a fan of reading a lot of books, especially boring books, but "+
+    "reading books helps us to think and imaginate. I believe thinking and imaginating things is what promotes us to move forward. "+
+    "Speaking of which, the second on my list is new technologies. Apart from being a computer science major, I love new technologies that are being created right now. "+
+    ""+
     "<br><br>"+
-    "I used to hate traveling as a kid, because I was afraid of going new places, but now I love to travel. Although it is hard to find the time to travel nowadays, compared to when I just entered college, whenever I do, I try to go places I haven't been. "+ 
-    "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
+    "";
     
     document.getElementById("how").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
     "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
