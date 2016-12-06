58a55821f473c6f98bc817f677f1a524d02cb89d
TB <youngkeu@buffalo.edu>
2016-12-06 10:25:13 -0500

    edited the projects title

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
 
 
 function myBio(){
-    document.getElementById("who").innerHTML = "My name is Thomas Youngkeun Bong. My Korean name is 봉영근(Bong-Young-Keun). I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
-    "<br><br>"+
+    document.getElementById("who").innerHTML = "My name is Thomas Youngkeun Bong. My Korean name is 봉영근(Bong-Young-Keun). "+
     "In my sparetime, I like working out or playing sports. I like being active. I've been playing soccer since I was a kid, and I've been playing tennis since High School. "+
     "If I'm being very lazy, I'd watch movies all day. My favorite movie/book is Harry Potter. Watching Harry Potter movies makes me happy for some reason... "+
     "J.K Rowling is a genius. Who doesn't love Harry Potter, right?";
@@ -35,9 +33,6 @@ function myBio(){
     "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
     
     document.getElementById("when").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
-    "<br><br>"+
-    "I used to hate traveling as a kid, because I was afraid of going new places, but now I love to travel. Although it is hard to find the time to travel nowadays, compared to when I just entered college, whenever I do, I try to go places I haven't been. "+ 
-    "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
+    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. ";
 }
 myBio();
