60fb704a4da3f78ae29ca7901afa9197235385eb
TB <youngkeu@buffalo.edu>
2016-12-06 10:51:53 -0500

    added more paragraphs on background page

--- background.js
+++ background.js
@@ -6,33 +6,21 @@ function myBio(){
     "If I'm being very lazy, I'd watch movies all day. My favorite movie/book is Harry Potter. Watching Harry Potter movies makes me happy for some reason... "+
     "J.K Rowling is a genius. Who doesn't love Harry Potter, right?";
 
-    document.getElementById("what").innerHTML = "There are lots of things that interest me. From the top of my head, the first on my list is "+
-    "movies/books. Without movies/books, we are nothing. There would not be any imagination or education. To be honest, I'm not a fan of reading a lot of books, especially boring books, but "+
-    "reading books helps us to think and imaginate. I believe thinking and imaginating things is what promotes us to move forward. "+
-    "Speaking of which, the second on my list is new technologies. Apart from being a computer science major, I love new technologies that are being created right now. "+
-    ""+
-    "<br><br>"+
-    "";
+    document.getElementById("what").innerHTML = "Apart from Computer Science, I like sports, movies, hanging out with my friends. "+
+    "Everyone around me gives me a great support and I cannot ever thank them enough. Especially my family and my closest friends, "+
+    "without whom I would not have been the person I am today.";
     
-    document.getElementById("how").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
-    "<br><br>"+
-    "I used to hate traveling as a kid, because I was afraid of going new places, but now I love to travel. Although it is hard to find the time to travel nowadays, compared to when I just entered college, whenever I do, I try to go places I haven't been. "+ 
-    "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
+    // document.getElementById("how").innerHTML = ""
     
-    document.getElementById("why").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
-    "<br><br>"+
-    "I used to hate traveling as a kid, because I was afraid of going new places, but now I love to travel. Although it is hard to find the time to travel nowadays, compared to when I just entered college, whenever I do, I try to go places I haven't been. "+ 
-    "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
+    document.getElementById("why").innerHTML = "Having fun is the most important thing in life. Of course, you cannot always have fun, but with "+
+    "positive attitude, you can always try to have fun. My father used to say, 'if you need to do something, don't complain. Enjoy them. You have to do it anyway. "+
+    "I used to detest hearing that as a kid, but now I agree. ";
     
-    document.getElementById("where").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. "+
-    "<br><br>"+
-    "I used to hate traveling as a kid, because I was afraid of going new places, but now I love to travel. Although it is hard to find the time to travel nowadays, compared to when I just entered college, whenever I do, I try to go places I haven't been. "+ 
-    "Seeing and experiencing new things helps to appreciate how big this world is and how different we all are. ";
+    document.getElementById("where").innerHTML = "My real home is Suwon, South Korea. I was born there and spent most of my childhood. "+
+    "Then, I moved to Lansing, NY when I was 13. It was a lot for a 13 year old to take in, moving to the other end of the world, but again, the people in Lansing were very kind to me, and everyone welcomed me. "+
+    "After graduating High School, I came to Buffalo, NY. I must be tapped with God's most blessed stick, because everywhere I go, I find myself with the most amazing people. ";
     
-    document.getElementById("when").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
-    "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. ";
+    // document.getElementById("when").innerHTML = "I grew up in Suwon, South Korea and moved to Ithaca, NY when I was thirteen. "+
+    // "New York has been my home since then. I am currently studying Computer Science at the University at Buffalo in Buffalo, NY, and I am graduating in December 2016. ";
 }
 myBio();
